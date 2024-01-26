26acec1a49727b45180de93632fa5fb205c9eb77
Joshua Agutu <128697219+joshking-vs@users.noreply.github.com>
2024-01-26 19:08:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yeah=document.querySelector(".yes");
+const nope=document.querySelector(".no");
+
+yeah.addEventListener('click',function(){
+    alert("obviously you love me😙");
+})
+nope.addEventListener('click',function(){
+    alert("hunipendi msupa☹");
+})
